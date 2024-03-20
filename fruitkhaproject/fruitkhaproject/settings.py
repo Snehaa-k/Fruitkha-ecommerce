ee9b26bcbdce46af86520951f0bfc7a5883ef88d
sneha <snehaksnehak470@gmail.com>
2024-03-20 13:01:11 +0530

    st

--- fruitkhaproject/fruitkhaproject/settings.py
+++ fruitkhaproject/fruitkhaproject/settings.py
@@ -163,7 +163,7 @@ STATIC_ROOT = '/staticfiles'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "home", "static"),
     os.path.join(BASE_DIR, "adminpanel", "static"),
