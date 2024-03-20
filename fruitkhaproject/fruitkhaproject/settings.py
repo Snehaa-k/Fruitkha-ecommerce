b9d90a2390d7e6dcdfb22fd262daef5ded23898b
sneha <snehaksnehak470@gmail.com>
2024-03-20 10:24:11 +0530

    static removed

--- fruitkhaproject/fruitkhaproject/settings.py
+++ fruitkhaproject/fruitkhaproject/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "home", "static"),
     os.path.join(BASE_DIR, "adminpanel", "static"),
-    os.path.join(BASE_DIR, "products", "static"),
+    # os.path.join(BASE_DIR, "products", "static"),
     os.path.join(BASE_DIR, "category", "static"),
 ]
 
