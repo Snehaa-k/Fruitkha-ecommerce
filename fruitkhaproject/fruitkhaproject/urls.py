dcd86964182d37883b1e64810a6e9dbd6d7cee98
sneha <snehaksnehak470@gmail.com>
2024-03-20 12:49:54 +0530

    staticfiles

--- fruitkhaproject/fruitkhaproject/urls.py
+++ fruitkhaproject/fruitkhaproject/urls.py
@@ -29,3 +29,5 @@ urlpatterns = [
     path("category/", include("category.urls")),
     path("cart/", include("cart.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
